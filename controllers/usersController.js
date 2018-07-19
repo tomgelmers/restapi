c677c66dd6d6b3293c63dc701e73c99018b538a8
Tom <tgelmers@avans.nl>
2018-07-20 00:09:15 +0200

    Episode 8

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,8 @@
 const userModel = require('../models/userModel');
 const carModel = require('../models/carModel');
 
+
+
 module.exports = {
     index: async (req, res, next) => {
         let users = await userModel.find({});
@@ -14,13 +16,13 @@ module.exports = {
     },
 
     getUser: async (req, res, next) => {
-        let { userId } = req.params;
+        let { userId } = req.value.params;
         let user = await userModel.findById(userId);
         res.status(200).json(user);
     },
 
     replaceUser: async (req, res, next) => {
-        let { userId } = req.params;
+        let { userId } = req.value.params;
         let newUser = req.body;
         await userModel.findByIdAndUpdate(userId, newUser);
 
@@ -28,27 +30,26 @@ module.exports = {
     },
 
     updateUser: async (req, res, next) => {
-        let { userId } = req.params;
+        let { userId } = req.value.params;
         let newUser = req.body;
         await userModel.findByIdAndUpdate(userId, newUser);
-
         res.status(200).json({success: true});
     },
 
     deleteUser: async (req, res, next) => {
-        let { userId } = req.params;
+        let { userId } = req.value.params;
         await userModel.findByIdAndRemove(userId);
         res.status(200).json({ success: true });
     },
 
     getUserCars: async (req, res, next) => {
-        let { userId } = req.params;
+        let { userId } = req.value.params;
         let user = await userModel.findById(userId).populate('cars');
         res.status(200).json( user.cars );
     },
 
     newUserCar: async (req, res, next) => {
-        let { userId } = req.params;
+        let { userId } = req.value.params;
         let user = await userModel.findById(userId);
         let newCar = new carModel(req.body);
         newCar.seller = user;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,18 +2,19 @@ const express = require('express');
 const router = require('express-promise-router')();
 
 const usersController = require('../controllers/usersController');
+const { validateParam, schemas } = require('../helpers/routeHelpers');
 
 router.route('/')
     .get(usersController.index)
     .post(usersController.newUser);
 
 router.route('/:userId')
-    .get(usersController.getUser)
-    .put(usersController.replaceUser)
-    .patch(usersController.updateUser)
-    .delete(usersController.deleteUser);
+    .get(validateParam(schemas.idSchema, 'userId'), usersController.getUser)
+    .put(validateParam(schemas.idSchema, 'userId'), usersController.replaceUser)
+    .patch(validateParam(schemas.idSchema, 'userId'), usersController.updateUser)
+    .delete(validateParam(schemas.idSchema, 'userId'), usersController.deleteUser);
 
 router.route('/:userId/cars')
-    .get(usersController.getUserCars)
-    .post(usersController.newUserCar);
+    .get(validateParam(schemas.idSchema, 'userId'), usersController.getUserCars)
+    .post(validateParam(schemas.idSchema, 'userId'), usersController.newUserCar);
 module.exports = router;
